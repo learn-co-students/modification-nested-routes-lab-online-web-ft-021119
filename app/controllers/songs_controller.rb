456a739897875238b40e61c650595e8b72e37f61
Burak Kocak <burakkocak884@ghotmail.com>
2019-05-07 22:46:26 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,7 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
+     
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
         redirect_to artists_path, alert: "Artist not found"
@@ -25,7 +26,11 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist is not found"
+    else
+    @song = Song.new(artist_id: params[:artist_id])
+  end
   end
 
   def create
@@ -39,8 +44,21 @@ class SongsController < ApplicationController
   end
 
   def edit
+    if params[:artist_id]
+      @nested_route = true
+      artist = Artist.find_by(id: params[:artist_id])
+      if artist.nil?
+        redirect_to artists_path, alert: "Artist is not found"
+      else
+        @song = artist.songs.find_by(id: params[:id])
+        redirect_to artist_songs_path(artist), alert: "Song is not found"
+      end
+    else
+      @nested_route = false
+
     @song = Song.find(params[:id])
   end
+end
 
   def update
     @song = Song.find(params[:id])
@@ -64,7 +82,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,23 @@
 Rails.application.routes.draw do
-  resources :artists do
+  resources :artists, only: [:show, :index] do
     resources :songs, only: [:index, :show, :new, :edit]
   end
   resources :songs
+
+#songs
+
+#get 'songs/:id' , to: 'songs#show' as: 'song'
+ #resources :songs, only:[:show]
+#resources :songs, except:[:destroy, :edit]
+
+ #artists < songs
+ #artists/:id/songs/:id
+ # get 'artists/:artist_id/songs', to: 'songs#index' , as: 'artist_songs'
+ # get 'artists/:artist_id/songs/:id', to: 'songs#index' , as: 'artist_song'
+
+
+
 end
+
+
+
